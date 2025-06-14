0f0ddd5c47cc98450c0d3f652fee40ce85cd7afa
isaack <isaackaris.52@gmail.com>
2025-06-14 12:32:50 +0300

    expense tracker

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css';
+
+const App = () => {
+  return (
+    <div className='text-3xl text-purple-700 '>App
+      
+    </div>
+  )
+}
+
+export default App
